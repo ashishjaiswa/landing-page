8883d72edc6c99a5a52a4aeb4018b1d84a2a4e30
ashish jaiswal <ashishjaiswa1996@gmail.com>
2024-11-05 16:20:48 +0530

    build push

---
+++ .next/server/pages/_app.js
@@ -0,0 +1,1 @@
+(()=>{var e={};e.id=888,e.ids=[888],e.modules={3893:(e,r,s)=>{"use strict";s.r(r),s.d(r,{default:()=>u});var t=s(997);s(2786);let u=({Component:e,pageProps:r})=>(0,t.jsx)(e,{...r})},2786:()=>{},997:e=>{"use strict";e.exports=require("react/jsx-runtime")}};var r=require("../webpack-runtime.js");r.C(e);var s=r(r.s=3893);module.exports=s})();
