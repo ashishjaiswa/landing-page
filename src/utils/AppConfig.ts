cf36787ec3dc66e6e2074bdb73185e52223a7677
ashish jaiswal <ashishjaiswa1996@gmail.com>
2024-11-05 17:30:56 +0530

    updaate

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'ATC',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  title: 'Home',
+  description: 'Ashoka consultancy services',
   locale: 'en',
 };
